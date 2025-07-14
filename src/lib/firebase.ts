347e61abb4dbe288756715cfff2ee87774bededf
Mouaad Idoufkir <mouaadidoufkir2@gmail.com>
2025-07-14 13:09:44 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDldETQGByoRgmYshMJ84AnfpKXpqAv

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDldETQGByoRgmYshMJ84AnfpKXpqAvsEM",
+  authDomain: "compass-6b774.firebaseapp.com",
+  projectId: "compass-6b774",
+  storageBucket: "compass-6b774.appspot.com",
+  messagingSenderId: "1029205662249",
+  appId: "1:1029205662249:web:336024502c69375042da38"
 };
 
 // Initialize Firebase
